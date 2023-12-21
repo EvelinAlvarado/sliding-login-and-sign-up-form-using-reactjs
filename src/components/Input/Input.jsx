5855fda87aed218b9bf04cf679ad3bb20215783e
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2023-12-21 21:52:48 +0000

    Implemented button functionality to handle Sign Up and Sign In actions

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,8 +1,13 @@
 import "./Input.css";
 
 function Input(props) {
+  /* cuando action es igual a Sign in y props title es igual a name, adicionar una clase para aplicar el estilo display:none class=inputHidden*/
+  const classNameHidden =
+    props.action === "Sign In" && props.title === "Name"
+      ? "form-input input-hidden"
+      : "form-input";
   return (
-    <div className="form-input">
+    <div className={classNameHidden}>
       <label className="form-input-label" for={props.forId}>
         {props.icon}
         {props.title}
